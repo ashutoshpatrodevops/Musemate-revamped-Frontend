7c6ff2dd3404c1c90be9418ad6fcb1140fc9a350
ashutoshpatrodevops <ashudevops7847@gmail.com>
2026-03-15 17:01:46 +0530

    integration 1st commit

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
       },
       {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
